27593e861c85dc347af30c3ea93894086aebba14
wufeng <wufeng4728704@hotmail.com>
2022-07-13 10:56:19 +0800

    add new demo

---
+++ MauiApp3/MauiDesignControl/AppHostExtesions.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiDesignControl;
+
+// All the code in this file is included in all platforms.
+public static class AppHostExtesions
+{
+    public static MauiAppBuilder ConfigureCustomControls(this MauiAppBuilder builder, bool useCompatibilityRenderers = false)
+    {
+        builder
+            .ConfigureMauiHandlers(handlers =>
+            {
+                 handlers.AddLibraryHandlers();
+            });
+
+        return builder;
+    }
+
+    public static IMauiHandlersCollection AddLibraryHandlers(this IMauiHandlersCollection handlers)
+    {
+        //handlers.AddTransient(typeof(CustomCheckBox), h => new CustomCheckBoxHandler());
+
+        return handlers;
+    }
+}
---
+++ MauiApp5/MauiApp5/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿#if IOS || ANDROID || MACCATALYST
+using Microsoft.Maui.Graphics.Controls.Hosting;
+#endif
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+
+#if IOS || ANDROID || MACCATALYST
+            .ConfigureGraphicsControls()
+#endif
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
